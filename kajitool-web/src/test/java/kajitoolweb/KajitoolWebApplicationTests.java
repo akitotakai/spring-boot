9eabc829b4f59659dd575e3de84dc8ad47e80b7f
akitotakai <takai1414@icloud.com>
2022-05-03 21:32:51 +0900

    spring initializar 追加

---
+++ kajitool-web/src/test/java/kajitoolweb/KajitoolWebApplicationTests.java
@@ -0,0 +1,13 @@
+package kajitoolweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KajitoolWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
